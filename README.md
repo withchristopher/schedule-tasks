cc2c419f753540a3fba5d0f87c81b32ab62b8406
Anita Ganti <65381336+anitapeppercorn@users.noreply.github.com>
2020-06-19 22:50:53 -0700

    Files for WorkSchedule

---
+++ README.md
@@ -0,0 +1,9 @@
+# Work-Day-Scheduler
+Creating a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Link to working App
+
+
+
+## Description
+This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
---
+++ script.js
@@ -0,0 +1,88 @@
+
+// Declaration of initial variables
+var currentDay = $("#currentDay");
+var scheduleArea = $(".schedule");
+var timeRow = $(".time-row");
+var currentDate = moment().format("dddd, MMMM Do");
+var currentHour = moment().format("H");
+var toDoItems = [];
+
+// An array of objects
+function startSchedule(){
+
+    timeRow.each(function(){
+    var thisRow = $(this);
+    var thisRowHr = parseInt(thisRow.attr("data-hour"));
+
+    var todoObj = {
+      hour: thisRowHr,
+      text: "",
+    }
+    toDoItems.push(todoObj);
+  });
+  // Loop all rows, save to local storage
+  localStorage.setItem("todos", JSON.stringify(toDoItems)); 
+};
+
+function saveIt(){
+  var hourToUpdate = $(this).parent().attr("data-hour");
+  var itemToAdd = (($(this).parent()).children("textarea")).val(); 
+  for (var j = 0; j < toDoItems.length; j++){
+    if (toDoItems[j].hour == hourToUpdate){
+     
+      toDoItems[j].text = itemToAdd;
+    }
+  }
+  localStorage.setItem("todos", JSON.stringify(toDoItems));
+  renderSchedule();
+}
+
+//format the rows colors depending on time
+function setUpRows(){
+  timeRow.each(function(){
+  var thisRow = $(this);
+  var thisRowHr = parseInt(thisRow.attr("data-hour"));
+
+  // style rows to show where we are in the day
+  if (thisRowHr == currentHour) {
+    thisRow.addClass("present").removeClass("past future");
+  }
+  if (thisRowHr < currentHour) {
+    thisRow.addClass("past").removeClass("present future");
+  }
+  if (thisRowHr > currentHour) {
+    thisRow.addClass("future").removeClass("past present");
+  }
+});
+}
+
+function renderSchedule(){
+  
+  toDoItems = localStorage.getItem("todos");
+  toDoItems = JSON.parse(toDoItems);
+    
+  for (var i = 0; i < toDoItems.length; i++){
+    var itemHour = toDoItems[i].hour;
+    var itemText = toDoItems[i].text; 
+   
+    $("[data-hour=" + itemHour + "]").children("textarea").val(itemText);
+  }
+}
+$(document).ready(function(){
+  setUpRows();
+
+if(!localStorage.getItem("todos")){
+  //initialize the array of objects
+  startSchedule();
+} //otherwise we will get it from local storage
+
+//display current date
+currentDay.text(currentDate);
+
+//render schedule from local storage
+renderSchedule();
+//when a todo item save button is clicked, save it
+scheduleArea.on("click", "button", saveIt);
+
+});
+
